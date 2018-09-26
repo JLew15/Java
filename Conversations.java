fc7bd6bc9e7a53fc16d0c2116cc1be43699af927
Jaiden Lewis <jaiden.lewis@tooeleschools.org>
2018-09-26 09:45:34 -0600

    Third Commit

--- Conversations.java
+++ Conversations.java
@@ -6,24 +6,21 @@ public static void main(String[] args) {
   Scanner input =  new Scanner(System.in);
   System.out.print("Please enter your name: ");
   String username = input.nextLine();
-  Scanner input =  new Scanner(System.in);
   System.out.print("Please enter a noun: ");
   String noun = input.nextLine();
-  Scanner input =  new Scanner(System.in);
   System.out.print("Please enter a verb: ");
   String verb = input.nextLine();
-  Scanner input =  new Scanner(System.in);
   System.out.print("Please enter an interger: ");
-  int interger = input.nextLine();
-  Scanner input =  new Scanner(System.in);
+  String interger = input.nextLine();
   System.out.print("Please enter a real (decimal) number: ");
-  double decimal = input.nextLine();
+  String decimal = input.nextLine();
 
   String story = "One day, " + username + " picked up a " + noun;
   story += " and decided to " + verb + " it.\n";
   story += "After getting arrested, spending " + interger;
   story += " days in jail, and paying a $" + decimal + " fine,\n";
-  story += name + " decided that was a bad idea!";
+  story += username + " decided that was a bad idea!";
+  System.out.println(story);
 
 
 }
