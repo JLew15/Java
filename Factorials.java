7f4ed91464def6c242f75781d87f7ba51a526595
Jaiden Lewis <jaiden.lewis@tooeleschools.org>
2018-10-04 10:15:16 -0600

    Third Commit

---
+++ Factorials.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Factorials {
+  public static void main(String[] args) {
+    Scanner input =  new Scanner(System.in);
+    System.out.print("Input a number: ");
+    int number = input.nextInt();
+
+    if (input.hasNextInt() == false) {
+      System.out.println("Please try again!");
+      input.close();
+      return;
+    }
+
+    
+  }
+
+}
