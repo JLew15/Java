792c6ab800879d7893f7a41d73b7face9b98c32f
Jaiden Lewis <jaiden.lewis@tooeleschools.org>
2018-10-15 08:48:55 -0600

    I MAED A SMILY

---
+++ Functions.java
@@ -0,0 +1,18 @@
+public class Functions {
+  public static void main(String[] args) {
+    makeSmiley();
+  }
+  public static void makeSmiley() {
+   System.out.println("        ***        ");
+   System.out.println("      **   **      ");
+   System.out.println("     *       *     ");
+   System.out.println("    *  *   *  *    ");
+   System.out.println("   *           *   ");
+   System.out.println("   *     *     *   ");
+   System.out.println("   *  *     *  *   ");
+   System.out.println("    *  *****  *    ");
+   System.out.println("     **     **     ");
+   System.out.println("        ***        ");
+  }
+
+}
