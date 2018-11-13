af382bb44a45e3ec71846226fbc80d31047ed631
JLew15 <zombears@icloud.com>
2018-11-13 09:27:29 -0700

    Checkerboard.java added

---
+++ Checkerboard.java
@@ -0,0 +1,10 @@
+public class Checkerboard
+public static int printCheckerboard(int rows, int columns){
+  
+
+}
+public static void main(String[] args) {
+  printCheckerboard(3,5);
+  printCheckerboard(4,3);
+  printCheckerboard(8,8);
+}
