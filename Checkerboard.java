6a9e9513b51d3de5c848814a38a000c65b463e29
JLew15 <zombears@icloud.com>
2018-11-15 09:20:51 -0700

    HELLO

--- Checkerboard.java
+++ Checkerboard.java
@@ -1,10 +1,29 @@
-public class Checkerboard
-public static int printCheckerboard(int rows, int columns){
-  
+public class Checkerboard{
+public static int printCheckerboard(int numRows, int numCols){
+for (int row=0; row<numRows; row++)
+{
+  for (int col=0; col<numCols; col++)
+{
+  if (row % 2 == 0) {
+    System.out.print(" ");
+  }
+  else {
+    System.out.print("#");
+  }
+  if (col % 2 == 0) {
+    System.out.print(" ");
+  }
+  else {
+    System.out.print("#");
+  }
+
+}
+}
 
 }
 public static void main(String[] args) {
   printCheckerboard(3,5);
   printCheckerboard(4,3);
   printCheckerboard(8,8);
+}
 }
