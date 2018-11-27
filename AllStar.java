59025c533d591717e37eb5ce4416905654a4c562
JLew15 <zombears@icloud.com>
2018-11-27 09:29:28 -0700

    ALLSTAR HAS BEEN UPDATED TO LOOK AND WORK BETTER

--- AllStar.java
+++ AllStar.java
@@ -1,7 +1,8 @@
 public class AllStar{
   public static void main(String[] args) {
+    System.out.print("The years start coming");
     while (0 == 0) {
-      System.out.println("and they don't stop coming");
+      System.out.print(" and they don't stop coming");
 
     }
   }
