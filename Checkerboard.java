10e0dab0371d4cc871eaf8607854d0ce8c1c0db6
JLew15 <zombears@icloud.com>
2018-11-29 10:02:48 -0700

    CHECKERBOARD WORKS NOW

--- Checkerboard.java
+++ Checkerboard.java
@@ -2,28 +2,31 @@ public class Checkerboard{
 public static void printCheckerboard(int numRows, int numCols){
 for (int row=0; row<numRows; row++)
 {
+
+System.out.println("");
   for (int col=0; col<numCols; col++)
 {
-  if (row % 2 == 0) {
-    System.out.print(" ");
-  }
-  else {
+  if (row % 2 == 0 && col % 2 == 0){
     System.out.print("#");
   }
-  if (col % 2 == 0) {
+  else {
     System.out.print(" ");
   }
-  else {
+  if (row % 2 != 0 && col % 2 != 0){
     System.out.print("#");
   }
+  else {
+    System.out.print(" ");
+  }
+}
 
 }
-}
-  System.out.println("");
+System.out.println("");
 }
 public static void main(String[] args) {
   printCheckerboard(3,5);
   printCheckerboard(4,3);
   printCheckerboard(8,8);
+
 }
 }
