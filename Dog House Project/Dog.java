7b4651af3a48825f7db60532da1e3ef2d1279442
Jaiden Lewis <Zombears@icloud.com>
2018-12-10 16:38:36 -0700

    MADE DOG

---
+++ Dog House Project/Dog.java
@@ -0,0 +1,51 @@
+public class Dog{
+  private int numTimesFed = 0;
+  private boolean sleeping = false;
+  private String name = "Ruffer";
+
+  public String getName(){
+    return name;
+  }
+
+  public void wakeUp(){
+    System.out.println("WAKING UP");
+    sleeping = false;
+    numTimesFed = 0;
+  }
+
+  public void sleep(){
+    sleeping = true;
+    System.out.println("SLEEPING");
+  }
+
+  public void hear(String sound){
+    if(sleeping == true){
+      return;
+    }
+    if(sound == name){
+      System.out.println("WAG TAIL");
+    }
+    if(sound == "ding-dong"){
+      System.out.println("BARK");
+    }
+    else{
+      return;
+    }
+  }
+
+  public void feed(){
+    if(sleeping == true){
+      return;
+    }
+    else{
+      numTimesFed++;
+    }
+    if(numTimesFed >= 2){
+      System.out.println("YAWN");
+    }
+    else{
+      System.out.println("YUMMY");
+    }
+  }
+
+}
